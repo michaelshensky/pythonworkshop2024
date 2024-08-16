57b1661b57b1a598e488b46089851a805f69f147
Michael George Shensky <m.shensky@austin.utexas.edu>
2024-08-16 15:57:07 -0500

    Add comments to main script file
    Added comments to make the code more readable

--- pythonworkshop2024.py
+++ pythonworkshop2024.py
@@ -12,14 +12,15 @@ import matplotlib_venn
 
 print("all packages have been imported successfully!")
 
-
+#define the data
 groupone = set(["oak", "oak", "pecan", "walnut","bald cypress","sycamore"])
 grouptwo = set(["pecan","walnut","black locust","elm"])
 
+#create venn diagram
 matplotlib_venn.venn2((groupone, grouptwo), ("Park 1 Trees", "Park 2 Trees"))
 
 
-
+#show figure
 plt.show()
 
 
